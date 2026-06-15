e739065971858058a5b534bc623587e0a03e1f06
Shahriar Tasin <tasinshahriar90@gmail.com>
2026-06-15 12:21:04 +0600

    Initial commit - Week 1 and 2 foundations

---
+++ setup.bat
@@ -0,0 +1,10 @@
+@echo off
+echo Setting up AI Career workspace...
+mkdir week_04
+mkdir week_05
+mkdir week_06
+echo. > week_04\notes.txt
+echo. > week_05\notes.txt
+echo. > week_06\notes.txt
+echo Setup complete!
+dir
