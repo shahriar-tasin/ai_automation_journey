e5768152da66945a6f7380fd44b2fd27f8642521
Shahriar Tasin <tasinshahriar90@gmail.com>
2026-06-22 13:08:01 +0600

    First python mini project

---
+++ week_03/mini_project_01.py
@@ -0,0 +1,25 @@
+agency_name="Tasin's AI Lab"
+owner="Shahriar Tasin"
+city="Chittagong"
+country="Bangladesh"
+income_goal=5000
+current_income=500
+avg_income_per_client=100
+services_count=5
+clients=5
+months_in_business=3
+
+
+print(f"Agency: {agency_name}")
+print(f"Owner: {owner}")
+print(f"Location: {city}, {country}")
+print("")
+print(f"Income Goal: ${income_goal}/month")
+print(f"Average Income/Client: ${avg_income_per_client}")
+print(f"Clients: {clients}")
+print(f"Services Count: {services_count}")
+print("")
+print(f"Total Income: ${clients*avg_income_per_client}")
+print(f"Income Gap: ${income_goal-current_income}")
+print("")     
+print(f"Months In Business: {months_in_business}")
---
+++ week_03/variables.py
@@ -0,0 +1,17 @@
+agency_name = "Tasin's AI Lab"
+owner = "Shahriar Tasin"
+city = "Chittagong"
+country = "Bangladesh"
+income_goal = 3000
+current_income = 0
+is_accepting_clients = True
+services_count = 4
+
+print(f"Agency:{agency_name}")
+print(f"Owner:{owner}")
+print(f"Location:{city}, {country}")
+print(f"Income Goal:{income_goal}/month")
+print(f"Current Income:{current_income}/month")
+print(f"Accepting Clints:{is_accepting_clients}")
+print(f"Services Offered:{services_count}")
+print(f"Income Gap:${income_goal-current_income}/month to reach goal")
